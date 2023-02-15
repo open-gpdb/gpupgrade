ba5ee42c75fa31a5e42d26203e861723bf4ac507
Kalen Krempely <kalensk@gmail.com>
2023-02-15 15:33:08 -0800

    pg_upgrade tests: expand external table tests
    Add external table tests for tables with serial column types to ensure
    attaching the sequence to the table succeeds. That is, Serial column
    types are dumped in components and added together. The CREATE TABLE is
    dumped, followed by the CREATE SEQUENCE, and then attached to the table
    column with ALTER TABLE ONLY public.foobar ALTER COLUMN b SET DEFAULT
    nextval('public.foobar_b_seq'::regclass);
    
    Until GPDB commit XXX the restore failed with “unsupported ALTER command
    for external table” causing pg_upgrade to fail. Specifically, the
    pg_dump SQL contains the following ALTER which is not supported for
    external tables: `ALTER TABLE ONLY public.foobar ALTER COLUMN b SET
    DEFAULT nextval('public.foobar_b_seq'::regclass);`
    
    Add additional tests for external tables with with user defined type
    containing default value, and external tables with dropped columns. See
    GPDB commit XXX.

--- test/acceptance/pg_upgrade/5-to-6/upgradeable_tests/target_cluster_regress/sql/external_table.sql
+++
@@ -1,7 +0,0 @@
--- Copyright (c) 2017-2023 VMware, Inc. or its affiliates
--- SPDX-License-Identifier: Apache-2.0
-
---------------------------------------------------------------------------------
--- Validate that the upgradeable objects are functional post-upgrade
---------------------------------------------------------------------------------
-SELECT * FROM r_t1;
