953e1680ebd83184cc06d738cef3f6459bf3737a
Kevin Yeap <kyeap@vmware.com>
2023-04-20 19:49:27 -0700

    Add tests for tables names with special characters
    Tables ending with '$' would fail upgrade when restoring metadata into
    the new cluster because we were using blank dollar syntax tags `$$` to
    escape special characters in table names. Tag was changed to `$_GPDB_$`
    to reduce the chance of collision. Add tests to ensure tables with
    special characters in their name can be upgraded.
    
    gpdb reference commit:
    https://github.com/greenplum-db/gpdb/commit/5fd62ea69ba12151ba42b293e02bb538d03f661c

---
+++ test/acceptance/pg_upgrade/5-to-6/upgradeable_tests/target_cluster_regress/sql/special_characters.sql
@@ -0,0 +1,50 @@
+-- Copyright (c) 2017-2023 VMware, Inc. or its affiliates
+-- SPDX-License-Identifier: Apache-2.0
+
+-- Test to ensure that tables with special characters can be upgraded.
+
+--------------------------------------------------------------------------------
+-- Create and setup upgradeable objects
+--------------------------------------------------------------------------------
+
+SELECT * FROM "foo$";
+SELECT * FROM "$$foo$";
+SELECT * FROM "my_table_@pple";
+SELECT * FROM "users!@#$%^&*()_+`-={}|[];':\"<>,.?/";
+SELECT * FROM "Café_Latté";
+SELECT * FROM "data_2021-09-25";
+SELECT * FROM "Sales@2023";
+SELECT * FROM "table_(parenthesis)";
+SELECT * FROM "Ελληνικά_τραπέζια";
+SELECT * FROM "table_with_underscores and spaces";
+SELECT * FROM "table_with_ö_umlaut";
+SELECT * FROM "table_with_की_hindi_characters";
+SELECT * FROM "学生表";
+
+INSERT INTO "foo$" (i) VALUES (1);
+INSERT INTO "$$foo$" (i) VALUES (2);
+INSERT INTO "my_table_@pple" (i) VALUES (3);
+INSERT INTO "users!@#$%^&*()_+`-={}|[];':\""<>,.?/" (i) VALUES (4);
+INSERT INTO "Café_Latté" (i) VALUES (5);
+INSERT INTO "data_2021-09-25" (i) VALUES (6);
+INSERT INTO "Sales@2023" (i) VALUES (7);
+INSERT INTO "table_(parenthesis)" (i) VALUES (8);
+INSERT INTO "Ελληνικά_τραπέζια" (i) VALUES (9);
+INSERT INTO "table_with_underscores and spaces" (i) VALUES (10);
+INSERT INTO "table_with_ö_umlaut" (i) VALUES (11);
+INSERT INTO "table_with_की_hindi_characters" (i) VALUES (12);
+INSERT INTO "学生表" (i) VALUES ('张三');
+
+SELECT * FROM "foo$";
+SELECT * FROM "$$foo$";
+SELECT * FROM "my_table_@pple";
+SELECT * FROM "users!@#$%^&*()_+`-={}|[];':\""<>,.?/";
+SELECT * FROM "Café_Latté";
+SELECT * FROM "data_2021-09-25";
+SELECT * FROM "Sales@2023";
+SELECT * FROM "table_(parenthesis)";
+SELECT * FROM "Ελληνικά_τραπέζια";
+SELECT * FROM "table_with_underscores and spaces";
+SELECT * FROM "table_with_ö_umlaut";
+SELECT * FROM "table_with_की_hindi_characters";
+SELECT * FROM "学生表";
