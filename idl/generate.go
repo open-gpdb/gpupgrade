36eb772fcc932d0bcd3e4176c35057279a00139d
Kevin Yeap <kyeap@vmware.com>
2023-07-12 10:15:24 -0700

    update grpc and mock compile commands and the proto files

--- idl/generate.go
+++ idl/generate.go
@@ -3,9 +3,14 @@
 
 package idl
 
-// Creates the .pb.go protobuf definitions.
-//go:generate protoc --plugin=../dev-bin/protoc-gen-go --go_out=plugins=grpc:. common.proto cli_to_hub.proto hub_to_agent.proto
+// Creates the .pb.go protobuf definitions. The use of
+// require_unimplemented_servers=false is to use legacy grpc implementation
+// that does not care about forward compatibility. We are the sole users and
+// maintainers of this grpc definition so this works for us and lets us not
+// need to embed a non-functional interface in our hub and agent servers just
+// to meet a grpc requirement that does not apply to us.
+//go:generate protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=require_unimplemented_servers=false:. --go-grpc_opt=paths=source_relative common.proto cli_to_hub.proto hub_to_agent.proto
 
 // Generates mocks for the above definitions.
-//go:generate ../dev-bin/mockgen -destination mock_idl/mock_cli_to_hub.pb.go github.com/greenplum-db/gpupgrade/idl CliToHubClient,CliToHubServer,CliToHub_ExecuteServer,CliToHub_ExecuteClient
-//go:generate ../dev-bin/mockgen -source hub_to_agent.pb.go -destination mock_idl/mock_hub_to_agent.pb.go
+//go:generate ../dev-bin/mockgen -source cli_to_hub_grpc.pb.go -destination mock_idl/mock_cli_to_hub_grpc.pb.go
+//go:generate ../dev-bin/mockgen -source hub_to_agent_grpc.pb.go -destination mock_idl/mock_hub_to_agent_grpc.pb.go
