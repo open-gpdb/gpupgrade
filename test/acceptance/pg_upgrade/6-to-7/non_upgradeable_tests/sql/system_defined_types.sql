ed2c2743fb2c7c0f3920a0b9efb0672aa6e801d6
Kevin Yeap <kyeap@vmware.com>
2023-12-04 08:51:18 -0800

    nonupgradable test to look for tables that use system defined types
    System defined types are not guaranteed to have consistent oids when
    moving from on version of postgres to the next. Unfortunately, there are
    quite a few types that got their OID changed from GPDB6 (postgres 9.4)
    to GPDB7 (12.12). The query looking user table that use these types had
    to be rewritten for upgrade because the recursive query looking for
    these types of relations contained a self reference in a subquery. This
    specific type of query is disabled in 6x so it was rewritten in plpgsql.
    
    gpdb reference commit:
    https://github.com/greenplum-db/gpdb/pull/16820

---
+++ test/acceptance/pg_upgrade/6-to-7/non_upgradeable_tests/sql/system_defined_types.sql
@@ -0,0 +1,76 @@
+-- Copyright (c) 2017-2023 VMware, Inc. or its affiliates
+-- SPDX-License-Identifier: Apache-2.0
+
+-- System defined types are not guaranteed to have consistent oids when moving
+-- from on version of postgres to the next. The query that looks for these
+-- types had to be rewritten for upgrade because the recursive query looking
+-- for these types of relations contained a self reference in a subquery. This
+-- specific type of query is disabled in 6x so it was rewritten in plpgsql.
+
+--------------------------------------------------------------------------------
+-- Create and setup non-upgradeable objects
+--------------------------------------------------------------------------------
+CREATE TABLE table_using_system_defined_type (
+	pg_type_column pg_type
+);
+
+-- build custom types that depend on each other to test recursive query used to
+-- find the tables that depend on system-defined types.
+CREATE TYPE custom_type_using_system_defined AS (
+	id int,
+	t0 pg_type
+);
+CREATE TYPE arr_custom_type1 AS (
+	id int,
+	t1 custom_type_using_system_defined[]
+);
+CREATE TYPE arr_custom_type2 AS (
+	id int,
+	t2 arr_custom_type1[]
+);
+CREATE TYPE arr_custom_type3 AS (
+	id int,
+	t3 arr_custom_type2[]
+);
+CREATE TABLE table_using_multiple_layers_of_system_types (
+    id int,
+    ct0 custom_type_using_system_defined,
+    ct1 arr_custom_type1,
+    ct2 arr_custom_type2,
+    ct3 arr_custom_type3
+);
+
+CREATE TYPE custom_range_type_using_system_defined AS RANGE (
+    subtype = pg_type
+);
+CREATE TABLE table_using_system_defined_range (
+    id int,
+    custom_range custom_range_type_using_system_defined NOT NULL
+);
+
+CREATE DOMAIN custom_domain_type_using_system_defined AS custom_range_type_using_system_defined;
+CREATE TABLE table_using_system_defined_domain (
+    id int,
+    custom_domain custom_domain_type_using_system_defined NOT NULL
+);
+
+---------------------------------------------------------------------------------
+--- Assert that pg_upgrade --check correctly detects the non-upgradeable objects
+---------------------------------------------------------------------------------
+!\retcode gpupgrade initialize --source-gphome="${GPHOME_SOURCE}" --target-gphome=${GPHOME_TARGET} --source-master-port=${PGPORT} --disk-free-ratio 0 --non-interactive;
+! cat ~/gpAdminLogs/gpupgrade/pg_upgrade/p-1/tables_using_composite.txt;
+
+---------------------------------------------------------------------------------
+--- Workaround to unblock upgrade
+---------------------------------------------------------------------------------
+DROP TABLE table_using_system_defined_domain;
+DROP TABLE table_using_system_defined_range;
+DROP TABLE table_using_multiple_layers_of_system_types;
+DROP TABLE table_using_system_defined_type;
+
+DROP TYPE custom_domain_type_using_system_defined;
+DROP TYPE custom_range_type_using_system_defined;
+DROP TYPE arr_custom_type3;
+DROP TYPE arr_custom_type2;
+DROP TYPE arr_custom_type1;
+DROP TYPE custom_type_using_system_defined;
