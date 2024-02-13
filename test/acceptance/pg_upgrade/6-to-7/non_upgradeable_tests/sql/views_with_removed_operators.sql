5bd5a83ed51abdb1e780b82f65fe64cff57a7a84
Kevin Yeap <kyeap@vmware.com>
2024-02-12 18:18:35 -0800

    pg_upgrade nonupgradable test detecting views with removed operators
    Views that use removed operators will cause upgrade to fail. This
    happens during metadata restore on the target cluster because pg_restore
    will error trying to create a view using types that do not exist
    anymore. This is not ideal as we could be many hours into upgrade before
    a view using a removed operator causes pg_upgrade to fail. Test that
    pg_upgrade is looking for views using removed operators.

---
+++ test/acceptance/pg_upgrade/6-to-7/non_upgradeable_tests/sql/views_with_removed_operators.sql
@@ -0,0 +1,23 @@
+-- Copyright (c) 2017-2023 VMware, Inc. or its affiliates
+-- SPDX-License-Identifier: Apache-2.0
+
+--------------------------------------------------------------------------------
+-- Create and setup non-upgradeable objects
+--------------------------------------------------------------------------------
+
+DROP SCHEMA IF EXISTS removed_operators CASCADE;
+CREATE SCHEMA removed_operators;
+SET search_path to removed_operators;
+
+CREATE OR REPLACE VIEW view_with_int2vectoreq AS SELECT '1 2'::INT2VECTOR = '1 2'::INT2VECTOR;
+
+---------------------------------------------------------------------------------
+--- Assert that pg_upgrade --check correctly detects the non-upgradeable objects
+---------------------------------------------------------------------------------
+!\retcode gpupgrade initialize --source-gphome="${GPHOME_SOURCE}" --target-gphome=${GPHOME_TARGET} --source-master-port=${PGPORT} --disk-free-ratio 0 --non-interactive;
+! cat ~/gpAdminLogs/gpupgrade/pg_upgrade/p-1/views_with_removed_operators.txt;
+
+---------------------------------------------------------------------------------
+--- Workaround to unblock upgrade
+---------------------------------------------------------------------------------
+DROP VIEW view_with_int2vectoreq;
