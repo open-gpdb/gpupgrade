a76f084ead4af30e37d7d4eff3003fff5a7ccee1
Marbin Tan <tmarbin@vmware.com>
2024-03-15 10:27:09 -0700

    Add non-upgradable tests for disallowed operator =>
    Postgres 9.5 and above no longer allows operator => to be created by the users.
    Upgrading from GP6 (9.4) to GP7 will fail due to this.
    
    For example, when running the command `CREATE OPERATOR =>`, the user will see the following
    error: `ERROR:  syntax error at or near "=>"`.
    
    So during metadata restore on the target cluster, pg_restore will
    error trying to create an operator that is no longer allowed.
    This check ensures that the user is alerted that they have an operator
    that is no longer allowed.

---
+++ test/acceptance/pg_upgrade/6-to-7/non_upgradeable_tests/sql/disallowed_pg_operator.sql
@@ -0,0 +1,24 @@
+-- Copyright (c) 2017-2024 VMware, Inc. or its affiliates
+-- SPDX-License-Identifier: Apache-2.0
+
+--------------------------------------------------------------------------------
+-- Create and setup non-upgradeable objects
+--------------------------------------------------------------------------------
+CREATE OPERATOR => (leftarg = int8, procedure = numeric_fac);
+
+CREATE DATABASE test_disallowed_pg_operator;
+1:@db_name test_disallowed_pg_operator:CREATE OPERATOR => (leftarg = int8, procedure = numeric_fac);
+1q:
+
+---------------------------------------------------------------------------------
+--- Assert that pg_upgrade --check correctly detects the non-upgradeable objects
+---------------------------------------------------------------------------------
+!\retcode gpupgrade initialize --source-gphome="${GPHOME_SOURCE}" --target-gphome=${GPHOME_TARGET} --source-master-port=${PGPORT} --disk-free-ratio 0 --non-interactive;
+! cat ~/gpAdminLogs/gpupgrade/pg_upgrade/p-1/databases_with_disallowed_pg_operator.txt;
+
+---------------------------------------------------------------------------------
+--- Cleanup
+---------------------------------------------------------------------------------
+
+DROP OPERATOR => (bigint, NONE);
+DROP DATABASE test_disallowed_pg_operator;
